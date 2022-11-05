be2f52ff4096b4c36b1b98dd5def7df93430da57
asaddoost <asad.doost@gmail.com>
2022-11-05 21:54:43 +0330

    08 added

---
+++ 08-sidebar-01/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Sidebar from './components/Sidebar';
+import 'boxicons'
+import './components/SidebarData'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
---
+++ 08-sidebar-01/src/components/SidebarData.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+export const SidebarData = [
+    {
+        title:"Home",
+        icon: <box-icon name='home-smile' color="bisque"></box-icon>,
+        link:"/home"
+    },
+    {
+        title:"Mailbox",
+        icon: <box-icon name='envelope'  color="bisque"></box-icon>,
+        link:"/mailbox"
+    },
+    {
+        title:"Analytics",
+        icon: <box-icon type='solid' name='bank' color="bisque"></box-icon>,
+        link:"/analytics"
+    },
+    {
+        title:"Dashboard",
+        icon: <box-icon name='dashboard' type='solid'  color="bisque"></box-icon>,
+        link:"/dashboard"
+    },
+    {
+        title:"Groups",
+        icon: <box-icon name='group'  color="bisque"></box-icon>,
+        link:"/group"
+    },
+    {
+        title:"Images",
+        icon: <box-icon name='images'  color="bisque"></box-icon>,
+        link:"/images"
+    },
+]
+
+export default SidebarData
