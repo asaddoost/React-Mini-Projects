b8783ed5e3534d46cc0d82d7167cfebf56fa6d31
asaddoost <asad.doost@gmail.com>
2023-01-11 18:23:15 +0330

    13 added

---
+++ 13-weatherApp-01/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Weather from "./components/Weather";
+import Time from "./components/Time";
+import DateToday from "./components/Date";
+import Quote from "./components/Quote";
+
+function App() {
+  return (
+    <div className="App">
+     
+     <Weather />
+     <Time />
+     <DateToday/>
+     <Quote />
+    </div>
+  );
+}
+
+export default App;
